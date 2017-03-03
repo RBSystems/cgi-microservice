6f5ac05fed96055556555bd6aae3ee237f654879
Jesse Millar <hellojessemillar@gmail.com>
2017-03-03 15:28:33 -0700

    Adding a health endpoint

--- server.go
+++ server.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/byuoitav/authmiddleware"
 	"github.com/byuoitav/cgi-microservice/handlers"
+	"github.com/byuoitav/hateoas"
+	"github.com/jessemillar/health"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
@@ -18,6 +20,11 @@ func main() {
 
 	// Use the `secure` routing group to require authentication
 	secure := router.Group("", echo.WrapMiddleware(authmiddleware.Authenticate))
+
+	// GET requests
+	router.GET("/", echo.WrapHandler(http.HandlerFunc(hateoas.RootResponse)))
+
+	router.GET("/health", echo.WrapHandler(http.HandlerFunc(health.Check)))
 
 	secure.GET("/:address/volume/up", handlers.VolumeUp)
 	secure.GET("/:address/volume/down", handlers.VolumeDown)
